b1bef0b5e0739331110e3a4f389c56aab7caa63a
Kaiden Keiser <keiserk4@udayton.edu>
2024-10-23 11:55:25 -0400

    Switched cookieapp to one with separated routes and models

--- messenger-app/index.js
+++ messenger-app/index.js
@@ -1,149 +1,66 @@
-const express = require('express');
-const app = express();
-const cookieParser = require('cookie-parser');
-const BodyParser = require('body-parser');
-const multer = require('multer');
-const upload = multer();
-const session = require('express-session')
-const port = process.env.PORT || 3000;
+/*
+    NOTE: I do not include the middleware to handle the users array, 
+    since I use persistent storage in MongoDB instead of an array.
+*/
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+const express = require('express')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const upload = multer()
+const session =require('express-session')
+const cookieParser = require('cookie-parser')
+const app = express()
+const User = require('./models.js')
+const port = process.env.PORT || 3000
 
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({extended: true}));
-app.use(upload.array());
-app.use(cookieParser());
-app.use(session({secret: "Mellon"}));
+app.set('view engine', 'pug')
+app.set('views', './views')
 
-const uname = "weaverg7";
-const pword = "6Sr0yiwLb5RMonsJ";
-const cluster = "cluster0.awlhm";
-const dbname = "test";
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(upload.array())
+app.use(cookieParser())
+app.use(session({secret: "Mellon"}))
 
-const url = `mongodb+srv://${uname}:${pword}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const username = "keiserk4"
+const password = "Pgq1JHQRjBWkJE0D"
+const cluster = "clustertruck.pt24s"
+const dbname = ""
 
-const mongoose = require('mongoose');
-const mongoose_settings = {useNewUrlParser: true};
+const uri = `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Clustertruck`
 
-mongoose.connect(url, mongoose_settings);
-const dbconn = mongoose.connection;
-dbconn.on("error", console.error.bind(console, "connection error: "));
-dbconn.once("open", () => {
-    console.log("Connected successfully to MongoDB");
-});
+const mongoose = require('mongoose')
+const mongoose_settings = {}
 
-const userSchema = {
-    id: String,
-    password: String,
-};
-
-const User = mongoose.model("User", userSchema);
-
-app.use(async (req, res, next) => {
-    try {
-        // Fetch all users from the database
-        const allUsers = await User.find().select('id');
-        let cur_users = allUsers.map(user => user.id).join(' ');
-        console.log("Registered users:", cur_users);
-        if (req.session.user) {
-            console.log(`Current user: ${req.session.user.id}`);
-        } else {
-            console.log("Current user: Not set");
-        }
-    } catch (err) {
-        console.error("Error fetching users:", err);
-    }
-
-    next();
-});
-
-app.get('/signup', (req, res) => {
-    res.render('signup');
-});
-
-app.post('/signup', (req, res) => {
-    if (!req.body.id || !req.body.password) {
-        res.render('signup', {message: "Please enter both id and password!"});
-        return;
-    };
-
-    let user = User.find({ id: req.body.id});
-
-    console.log("<Signup> Find: ", user);
-    if (user === undefined || user === null) {
-        const newUser = new User({
-            id: req.body.id,
-            password: req.body.password,
-        });
-        db.users.insertOne(newUser);
-        req.session.user = newUser;
-        res.redirect('/protected_page');
-        return;
-    } else {
-        res.render('signup', {message: "User Already Exists! Login or chose another user id"});
-        return;
-    }
-});
-
-const checkSignIn = (req, res, next) => {
-    if(req.session.user) {
-        return next();
-    } else {
-        const err = new Error("Not logged in!");
-        err.status = 400;
-        return next(err);
-    }
-};
-
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-app.post('/login', (req, res) => {
-    if(!req.body.id || !req.body.password) {
-        res.render('login', {message: "Please enter both id and password"});
-        return;
-    }
-
-    let user = User.findOne({ id: req.body.id, password: req.body.password});
-
-    if (user === undefined || user === null) {
-        res.render('login', {message: "Invalid credentials!"});
-        return;
-    } else {
-        req.session.user = { id: user._id, username: user.username };
-        res.redirect('/protected_page');
-        return;
-    }
+mongoose.connect(uri, mongoose_settings)
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error: "))
+db.once("open", () => {
+    console.log("Connected successfully to MongoDB")
 })
 
-app.get('/logout', (req, res) => {
-    let user = req.session.user.id;
-    req.session.destroy( () => {
-        console.log(`${user} logged out.`)
-    });
-    res.redirect('/login');
-});
+app.use('/', async (req, res, next) => {
+    const allUsers = await User.find({}, 'id password')
+    console.log("Registered users:")
+    for (const user of allUsers) {
+        console.log(`${user.id}`)
+    }
+    currentUser = (req.session.user) ? req.session.user.id : null
+    if (currentUser) {
+        console.log(`Current user: ${currentUser}`)
+    } else {
+        console.log("Current user: Not set")
+    }
+    next()
+})
 
-app.get('/protected_page', checkSignIn, (req, res) => {
-    res.render('protected_page', {id: req.session.user.id});
-});
+const routes = require('./routes.js')
+app.use('/', routes)
 
 app.use('/protected_page', (err, req, res, next) => {
-    res.redirect('/login');
+    res.redirect('/login')
 })
 
-app.get('/', (req, res) => {
-    res.cookie('name', 'express').send('cookie set'); //sets name = express
-    console.log('Cookies: ', req.cookies);
-});
-
-app.get('/clear_cookie', function(req, res) {
-    res.clearCookie('name');
-    res.send('Cookie name cleared');
-});
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Cookie app listening on port ${port}`);
+})
