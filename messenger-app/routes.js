813284915f3dbb23c641b47df2c517f73523521a
honsakerz1 <zhonsaker@gmail.com>
2024-10-30 17:33:16 -0400

    add more logging but its still broken :(

--- messenger-app/routes.js
+++ messenger-app/routes.js
@@ -3,18 +3,20 @@ const router = express.Router();
 const User = require('./models.js');
 const password = require('./password.js');
 
+//signup page
 router.get('/signup', (req, res) => {
     res.render('signup')
 })
 
+//handle signup request
 router.post('/signup', async (req, res) => {
     if (!req.body.id || !req.body.password) {
         res.render('signup', {message: "Error: user id or password not entered"})
         return
     }
 
-    const user = await User.findOne({'id': req.body.id}, 'id password')
-
+    const user = await User.findOne({'username': req.body.id}, 'id password')
+    console.log(`Found user: ${user}`)
     console.log("<Signup> Find: ", req.body.id)
     if (user === undefined || user === null) {
         const salt = password.generateSalt();
@@ -34,20 +36,27 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+//login page
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
+//handle login request
 router.post('/login', async (req, res) => {
     if(!req.body.id || !req.body.password) {
         res.render('login', {message: "Error: user id or password not entered"})
         return
     }
-
-    const user = await User.findOne({'id': req.body.id}, 'id password')
-
+    //TODO fix this in signup post as well
+    //TODO dehash password so the conditional succeeds
+    const user = await User.findOne({'username': req.body.id}, 'username hashedPassword')
+    console.log(`Found user: ${user}`)
     console.log("<Login> Find: ", req.body.id)
-    if (user === undefined || user === null || req.body.password != user.password) {
+    const salted_input_pass = password.hashPassword(req.body.password, user.salt)
+    console.log(` user.salt ${ user.salt}`)
+    console.log(`IN: ${salted_input_pass}`)
+    console.log(`OUT ${user.hashedPassword}`)
+    if (user === undefined || user === null || salted_input_pass != user.hashedPassword) {
         res.render('login', {message: "Invalid credentials"})
         return
     } else {
@@ -55,8 +64,9 @@ router.post('/login', async (req, res) => {
         res.redirect('/protected_page')
         return
     }
-})
+});
 
+//logout page
 router.get('/logout', (req, res) => {
     let user = req.session.id
     req.session.destroy(() => {
@@ -75,6 +85,7 @@ const checkSignIn = (req, res, next) => {
     }
 }
 
+//render protected page
 router.get('/protected_page', checkSignIn, (req, res) => {
     res.render('protected_page', {id: req.session.id})
 })
