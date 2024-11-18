7f9d7e8650d9dbffb2dd6e86f7cd43475b8986ce
Garrett Weaver <gdweaver15@gmail.com>
2024-11-18 07:17:38 -0500

    Socket.io now prints in terminal and updates html

--- messenger-app/static/js/client.js
+++ messenger-app/static/js/client.js
@@ -2,13 +2,19 @@ const socket = io();
 
 const form = document.getElementById('form');
 const input = document.getElementById('input');
+const messages = document.getElementById('messages');
 
 form.addEventListener('submit', (e) => {
-    console.log("hit the function")
     e.preventDefault();
     if (input.value) {
-        console.log("message")
         socket.emit('chat message', input.value);
         input.value = '';
     }
 });
+
+socket.on('chat message', (msg) => {
+    const item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+})
